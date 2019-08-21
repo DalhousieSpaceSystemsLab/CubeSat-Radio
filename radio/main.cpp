1d98ebdc7dfd7bdbbb91244168370c2a6b5e87a2
nickhansenrf <47431195+nickhansenrf@users.noreply.github.com>
2019-08-21 11:57:49 -0300

    created radioHelper class
    less confusing to read but still not "best practice"

--- radio/main.cpp
+++ radio/main.cpp
@@ -2,174 +2,230 @@
 #define DEBUG_RADIO
 #include "main.hpp"
 
-//will need to port this to linux.
+//whole thing will need to port this whole thing to linux.
 //https://blog.mbedded.ninja/programming/operating-systems/linux/linux-serial-ports-using-c-cpp/
+//the main thing is how a virtual com port / serial device appears in linux (everything is a file)
 
-//expects parameters such as file name and encode rate
-int main(int argc, char *argv[])
-{
-    std::ifstream inputdata;
-    std::ofstream outputdata;
-    int rateflag=0;
-
+//what even is best practice anyways :)
+class RadioHelper{
+public:
+    //data
+    std::ifstream inputFile;
+    std::ofstream outputFile;
     std::streampos dpos;
-    char* tempdata =  new char[totalsize];
+    int rateFlag = 0;
+    char tempData[TOTALSIZE];
+    int cport_nr=5,        //  (COM6 on windows)
+    bdrate=9600;       // 9600 baud
+    //8 data bits, no parity, 1 stop bit
+    char mode[4]= {'8','N','1', 0}; //Must match the serial config of the nL400
+    std::vector<char> dataVec;
+    //could save some memory (>1kb) by only making one of these at runtime
+    ezpwd::RS<TOTALSIZE, HALFRATE> rsh;
+    ezpwd::RS<TOTALSIZE, QUARTERRATE> rsq;
+    ezpwd::RS<TOTALSIZE, TENTHRATE> rst;
 
+//methods
+int DUMMY_OPEN_COMPORT(){
+    //TODO: HOWEVER SERIAL COMS WORKS IN LINUX ON OUR HARDWARE
+    return 1;
+}
 
-    if(argc > 1){
-        //eventually some parsing of arguments will take place here
-        D(std::cout << "Args given: ");
-        for(int i=0; i<argc; ++i){
-            D(std::cout << argv[i] << std::endl);
+int DUMMY_CLOSE_COMPORT(){
+    return 1;
+}
+
+int DUMMY_SERIAL_SEND(){
+    return 1;
+}
+
+int DUMMY_SERIAL_RECEIVE(){
+    return 1;
+}
+
+//there is definitely a better way to achieve this
+int readFromFile(){
+    switch(rateFlag){
+    case 1:{
+        dataVec.reserve(HALFRATE);
+        inputFile.read(tempData, HALFRATE);
+
+        for(int i=0; i<HALFRATE; i++){
+            dataVec.push_back(tempData[i]);
         }
-        if(strcmp(argv[2],"1")==0){
-            D(std::cout << "halfrate selected\n";)
-            rateflag = 1;
+        break;
+    }
+    case 2:{
+        dataVec.reserve(QUARTERRATE);
+        inputFile.read(tempData, QUARTERRATE);
+        for(int i=0; i<QUARTERRATE; i++){
+            dataVec.push_back(tempData[i]);
         }
-        else if(strcmp(argv[2],"2")==0){
-            D(std::cout << "quarterrate selected\n";)
-            rateflag =2;
-        }
-        else if(strcmp(argv[2],"3")==0){
-            D(std::cout << "tenthrate selected\n";)
-            rateflag = 3;
-        }
-        else{
-            D(std::cout << "ERROR WRONG RATE";)
-            return 0;
+        break;
+    }
+    case 3:{
+        dataVec.reserve(TENTHRATE);
+        inputFile.read(tempData, QUARTERRATE);
+        for(int i=0; i<TENTHRATE; i++){
+            dataVec.push_back(tempData[i]);
         }
     }
+    case 4:{
+        dataVec.reserve(TOTALSIZE);
+        inputFile.read(tempData, TOTALSIZE);
+        for(int i=0; i<TENTHRATE; i++){
+            dataVec.push_back(tempData[i]);
+        }
+    }
+    default:{
+        D(std::cout << "error";)
+        return 0;
+    }
+    }//switch rateFlag
+    return 1;
+}//setRate method
+
+//once again working around using templates at runtime
+int encode(){
+    switch(rateFlag){
+    case 1:{
+        rsh.encode(dataVec);
+        break;
+    }
+    case 2:{
+        rsq.encode(dataVec);
+        break;
+    }
+    case 3:{
+        rst.encode(dataVec);
+        break;
+    }
+    case 4:{
+        D(std::cout << "receve only"<<std::endl;)
+        break;
+    }
+    default:{
+        D(std::cout << "error";)
+        return 0;
+    }
+    }//end switch
+    return 1;
+}//encode method
+
+int transmitRadio(){
+
+    for(unsigned i=0; i<dataVec.size(); i++){
+        if(DUMMY_SERIAL_SEND()){
+            D(std::cout << "\nData Transmitted!\n";)
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int receiveRadio(){
+    if(DUMMY_SERIAL_RECEIVE()){
+        return 1;
+    }
+    return 0;
+}
+
+};//RadioHelper Object
+
+
+
+//expects parameters such as file name and encode rate
+//USAGE:
+//arg 0: String indicating path of executable (default in windows)
+//arg 1: "1" = transmit or "0" = receive mode
+//arg 2: path/name of input file containing data to transmit/receive
+//arg 3: the FEC parity rate at which to transmit "1"=half "2"=quarter "3"=tenth
+//example: 1 test 1 will transmit the contents of file "test" at FEC rate 1/2
+int main(int argc, char *argv[])
+{
+    RadioHelper radioHelper;
+
+    if(argc > 1){
+        D(std::cout << "Args given: ");
+        for(int i=0; i<argc; ++i){
+            D(std::cout << argv[i] << std::endl;)
+        }
+        if(strcmp(argv[1],"0")==0){
+            D(std::cout << "Receive mode selected" << std::endl;)
+            radioHelper.rateFlag = 4; //Receiving works differently
+        }
+        else if(strcmp(argv[1],"1")==0){
+            D(std::cout << "Transmit mode selected" << std::endl;)
+if(strcmp(argv[3],"1")==0){
+    D(std::cout << "halfrate selected\n";)
+    radioHelper.rateFlag = 1;
+}
+else if(strcmp(argv[3],"2")==0){
+    D(std::cout << "quarterrate selected\n";)
+    radioHelper.rateFlag =2;
+}
+else if(strcmp(argv[3],"3")==0){
+    D(std::cout << "tenthrate selected\n";)
+    radioHelper.rateFlag = 3;
+}
+else{
+    D(std::cout << "ERROR WRONG RATE";)
+    return 0;
+}//rate if
+        }//transmit mode if
+        else{
+            D(std::cout << "ERROR WRONG MODE";)
+            return 0;
+        }
+    }//if args
     else{
-        D(std::cout << "Expecting Args, such as file name and rate";)
+        D(std::cout << "Expecting Args, USAGE: ...todo";)
         return 0;
     }
 
     //open input and output files
-    D(std::cout << "Opening File : " << argv[1] << std::endl;)
-    inputdata.open(argv[1], std::ios::in | std::ios::binary);
-    outputdata.open("output", std::ios::out);
+    D(std::cout << "Opening File : " << argv[2] << std::endl;)
+    radioHelper.inputFile.open(argv[2], std::ios::in | std::ios::binary);
+    radioHelper.outputFile.open("output", std::ios::out | std::ios::ate);
 
-    //open COMPORT 6 on windows
-    int cport_nr=5,        //  (COM6 on windows)
-    bdrate=9600;       // 9600 baud
-    //8 data bits, no parity, 1 stop bit
-    //same settings as arduino... and can be for nL400
-    char mode[]= {'8','N','1', 0};
-    if(RS232_OpenComport(cport_nr, bdrate, mode, 0))
+    //open serial communications
+    if(radioHelper.DUMMY_OPEN_COMPORT()==0)
     {
         D(std::cout << "Can not open comport\n";)
         return(0);
     }
 
-    std::vector<char> datavec;
-    switch(rateflag){
-    case 1:{
-        datavec.reserve(halfrate);
-        inputdata.read(tempdata, halfrate);
-        for(int i=0; i<halfrate; i++){
-            datavec.push_back(tempdata[i]);
-        }
-        break;
-    }
-    case 2:{
-        datavec.reserve(quarterrate);
-        inputdata.read(tempdata, quarterrate);
-        for(int i=0; i<quarterrate; i++){
-            datavec.push_back(tempdata[i]);
-        }
-        break;
-    }
-    case 3:{
-        datavec.reserve(tenthrate);
-        inputdata.read(tempdata, quarterrate);
-        for(int i=0; i<tenthrate; i++){
-            datavec.push_back(tempdata[i]);
-        }
-    }
-    case 4:{ //receve a command
-        datavec.reserve(totalsize);
-    }
-    default:{
-        D(std::cout << "error";)
-        return 0;
-    }
-    }
+    radioHelper.readFromFile();
+    D(std::cout << "File contents read into vector: " <<std::endl; )
+    D(std::cout << radioHelper.dataVec << std::endl;)
 
-    D(std::cout << "File contents read into vector: " ; )
+    radioHelper.encode();
+    D(std::cout << "Encrypted: " << radioHelper.dataVec << std::endl;)
 
-    D(std::cout << datavec << std::endl;)
+    radioHelper.transmitRadio();
 
-    ezpwd::RS<totalsize, halfrate> rsh;
-    ezpwd::RS<totalsize, quarterrate> rsq;
-    ezpwd::RS<totalsize, tenthrate> rst;
+    radioHelper.receiveRadio();
+    D(std::cout << "Receved with corruptions: " << " " << std::endl;)
+    radioHelper.dataVec.at(0)='x';
+    radioHelper.dataVec.at(1)='x';
+    radioHelper.dataVec.at(2)='x';
+    radioHelper.dataVec.at(5)='x';
+    radioHelper.dataVec.at(9)='x';
+    radioHelper.dataVec.at(11)='x';
+    radioHelper.dataVec.at(23)='x';
+    D(std::cout << radioHelper.dataVec << std::endl;)
 
-    switch(rateflag){
-    case 1:{
-        rsh.encode(datavec);
-        break;
-    }
-    case 2:{
-        rsq.encode(datavec);
-        break;
-    }
+    int errorNum = radioHelper.rsh.decode(radioHelper.dataVec);
+    D(std::cout<<"Errors Corrected: "<< errorNum <<std::endl;)
+    D(std::cout << radioHelper.dataVec << std::endl;)
 
-    case 3:{
-        rst.encode(datavec);
-        break;
-    }
-    default:{
-        D(std::cout << "error";)
-        return 0;
-    }
-    }
-    D(std::cout << "encrypted: " << datavec << std::endl;)
-    std::vector<char> receiveStr(255);
-
-    transmitRadio(datavec, cport_nr);
-
-    receiveStr = receiveRadio(receiveStr, cport_nr, bdrate, mode);
-
-
-    D(std::cout << "receved: " << receiveStr << std::endl;)
-    int errornum = rst.decode(receiveStr);
-    D(std::cout<<"Result: "<<errornum<<std::endl;)
-    for(unsigned i=0; i<receiveStr.size(); i++){
-        D(std::cout << receiveStr.at(i) << ' ,';)
-    }
-
-    RS232_CloseComport(5);
-    inputdata.close();
-    outputdata.close();
+    //perhaps a destructor
+    radioHelper.DUMMY_CLOSE_COMPORT();
+    radioHelper.inputFile.close();
+    radioHelper.outputFile.close();
     return 0;
 }
 
-/*how to functionalize this file read...?*/
-int readFromFile(char* dest, void* in, int rate){
-D(std::cout << "\n Args: " << &dest << "\n" << in << "\n" << rate << std::endl;)
 
-return 0;
-}
 
-int transmitRadio(std::vector<char> dataString, int cport_nr){
 
-    for(unsigned i=0; i<dataString.size(); i++){
-        if(RS232_SendByte(cport_nr, dataString[i])){
-            D(std::cout << "error transmitting";)
-        }
-    }
-
-    return 0;
-}
-
-std::vector<char> receiveRadio(std::vector<char> receiveStr, int cport_nr, int bdrate, const char* mode){
-    int n=0;
-    unsigned char buffer[255];
-
-    n = RS232_PollComport(cport_nr, buffer, 255);
-    if(n > 0){
-        for(int i=0; i<n; i++){
-            receiveStr[i]=buffer[i];
-        }
-    }
-    return receiveStr;
-}
--- radio/main.hpp
+++ radio/main.hpp
@@ -1,19 +1,19 @@
+#ifndef RADIO_H
+#define RADIO_H
 
-//moving these to a header causes problems. ):
 #include "lib/rs"
 #include "lib/timeofday"
 #include "lib/output"
-#include "lib/rs232.h"
 //standard stuff
 #include <iostream>
 #include <fstream>
 #include <string>
 
 //sizes for RS container for 1/2 encoding, and 3/4, 9/10
-#define totalsize 255
-#define halfrate 128
-#define quarterrate 191
-#define tenthrate 223
+#define TOTALSIZE 255
+#define HALFRATE 128
+#define QUARTERRATE 191
+#define TENTHRATE 223
 
 
 //debug macro thing
@@ -21,10 +21,59 @@
 #define D(x) x
 #else
 #define D(x)
-#endif
+#endif //debug macro
 
 
-int readFromFile(char* dest, void* in, int rate);
 
-int transmitRadio(std::vector<char> dataString, int cport_nr);
-std::vector<char> receiveRadio(std::vector<char> receiveStr, int cport_nr, int bdrate, const char* mode);
+struct AX25_Frame
+{
+
+  // Frame type (I, U or S)
+  char  type[1];
+
+  //--- Frame Start ---//
+
+  // Address Subfields
+  char  addr_dest[7];
+  char  addr_src[7];
+
+  // Control field
+  char  ctrl[1];
+  char  ctrl_long[2];
+
+  // Protocol Identifier (PID) field, if applicable
+  char  pid[1];
+
+  // Information (I) field
+  char  i[256];
+
+  // Frame Check Sequence (FCS) field
+  char  fcs[2];
+
+  //--- Frame End ---//
+
+}ax25_frame;
+
+/////////////////////////////////
+//        AX.25 Methods        //
+/////////////////////////////////
+
+//--- Returns frame count required to parse data of known size ---//
+unsigned int ax25_framecount
+(
+  size_t        data_len         // length in bytes of information to be parsed.
+);
+
+
+
+//-- Parse frames from data ---//
+int ax25_parse
+(
+  char        frame_type[1],  // frame type ('i', 'u' or 's').
+  size_t      data_len,       // length in bytes of information referenced by *data.
+  void        *data,          // points to information desired to be parsed into frames.
+  struct AX25_Frame *frames_out     // points to an array of AX25 frames of at least ax25_framec(data_len) long.
+);
+
+
+#endif //header guard
