d1e8b567f4a060fee49f0f6bfbb3ebbcfac96e2b
nickhansenrf <47431195+nickhansenrf@users.noreply.github.com>
2019-08-28 11:49:05 -0300

    removed old packet definition
    We aren't implementing AX.25, but rather own own packet structure which is TBD

--- radio/main.hpp
+++ radio/main.hpp
@@ -25,55 +25,13 @@
 
 
 
-struct AX25_Frame
+struct LORIS_Frame
 {
 
-  // Frame type (I, U or S)
-  char  type[1];
+  
 
-  //--- Frame Start ---//
+}loris_frame;//Frame End
 
-  // Address Subfields
-  char  addr_dest[7];
-  char  addr_src[7];
-
-  // Control field
-  char  ctrl[1];
-  char  ctrl_long[2];
-
-  // Protocol Identifier (PID) field, if applicable
-  char  pid[1];
-
-  // Information (I) field
-  char  i[256];
-
-  // Frame Check Sequence (FCS) field
-  char  fcs[2];
-
-  //--- Frame End ---//
-
-}ax25_frame;
-
-/////////////////////////////////
-//        AX.25 Methods        //
-/////////////////////////////////
-
-//--- Returns frame count required to parse data of known size ---//
-unsigned int ax25_framecount
-(
-  size_t        data_len         // length in bytes of information to be parsed.
-);
-
-
-
-//-- Parse frames from data ---//
-int ax25_parse
-(
-  char        frame_type[1],  // frame type ('i', 'u' or 's').
-  size_t      data_len,       // length in bytes of information referenced by *data.
-  void        *data,          // points to information desired to be parsed into frames.
-  struct AX25_Frame *frames_out     // points to an array of AX25 frames of at least ax25_framec(data_len) long.
-);
 
 
 #endif //header guard
